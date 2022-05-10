e6cf9e77417fb37acd72a5967bf2afb0e4d8ba23
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-05-10 21:11:54 +0700

    Update haori.js

--- plugins/haori.js
+++ plugins/haori.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
   let ext= `
-  Status : Online 
-Oke Sekarang Bisa（ ・∀・）
+  Status : *Online* 
+Oke Kak Sekarang Bisa（ ・∀・）
 `.trim()
-conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'Fachri Di Sini')).buffer(), ext, '𝙷𝚊𝚘𝚛𝚒 𝚂𝚒𝚊𝚙 𝙼𝚎𝚖𝚋𝚊𝚗𝚝𝚞 𝙺𝚊𝚖𝚞 (*´ω｀*)', 'Siap Fachri', 'Iya', 'Hallo Fachri', 'Hallo Fachri', m)
+conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'Fachri Di Sini')).buffer(), ext, 'FachriBotz Siap Membantu Kamu (*´ω｀*)', 'Siap Fachri', 'Iya', 'Hallo Fachri', 'Hallo Fachri', m)
 
 }
 handler.customPrefix = /^(tes|tess|test)$/i
