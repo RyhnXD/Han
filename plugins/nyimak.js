abf66356f25a8c7ce1c71a146582e6fad8406dc7
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-05-14 12:49:07 +0700

    Update nyimak.js

--- plugins/nyimak.js
+++ plugins/nyimak.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m) => {
 let stc = fs.readFileSync('./sticker/nyimak.webp')
-conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Haori Nyimak Aja Deh :v*', 'status@broadcast')
+conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Fachri Nyimak Aja Deh :v*', 'status@broadcast')
 }
 
 handler.customPrefix = /^(Nyimak|nyimak|Menyimak|menyimak)$/i
