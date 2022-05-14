6c9f5c9e9ea3aa891eb5d414427849bd7767e24c
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-05-14 12:45:09 +0700

    Update sabar.js

--- plugins/sabar.js
+++ plugins/sabar.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 let handler = async (m) => {
-let stc = fs.readFileSync('./src/sabar.webp')
+let stc = fs.readFileSync('./sticker/sabar.webp')
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*sabar*', 'status@broadcast')
 }
 
